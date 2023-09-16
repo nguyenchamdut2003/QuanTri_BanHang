1ba52245e493a4fba34a3c1e5d4434c92b378516
manh <manhtdph19414@fpt.edu.vn>
2023-09-16 12:44:20 +0700

    Mạnh(16/09)

--- app/src/main/java/com/example/quantri_banhang/fragment/fragment_taikhoan.java
+++ app/src/main/java/com/example/quantri_banhang/fragment/fragment_taikhoan.java
@@ -2,9 +2,11 @@ package com.example.quantri_banhang.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,16 @@ import androidx.fragment.app.Fragment;
 
 import com.example.quantri_banhang.R;
 import com.example.quantri_banhang.actitvity.LoginActivity;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class fragment_taikhoan extends Fragment {
-
+    GoogleSignInClient googleSignInClient;
     public fragment_taikhoan() {
         // Required empty public constructor
     }
@@ -39,10 +47,19 @@ public class fragment_taikhoan extends Fragment {
         view.findViewById(R.id.tv_LogOut).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+
                 FirebaseAuth.getInstance().signOut();
+                GoogleSignIn.getClient(
+                        getContext(),
+                        new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+                ).signOut();
+
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
                 startActivity(intent);
             }
         });
     }
+
 }
